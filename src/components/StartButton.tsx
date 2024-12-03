98674af68269f7e136ebeacea28cf832fdbdc422
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2024-12-03 17:52:29 -0500

    Working play button!!!

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import { MakeDeck, MakeHand } from "./Balatro";
+
 function Start() {
-  return (
-    <img className="ui" src="https://i.ibb.co/26vt9qk/markup-1000008762.png" height="75px"></img>
-  )
+  const [started, setStarted] = useState(false);
+  if (started) {
+    return <MakeHand deck={MakeDeck()} handsize={8} />
+  } else {
+    return (
+      <img
+        className="ui"
+        src="https://i.ibb.co/26vt9qk/markup-1000008762.png"
+        height="75px"
+        onClick={() => setStarted(true)}
+      ></img>
+    );
+  }
 }
 
 export default Start;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
-import { MakeDeck, MakeHand } from "./components/Balatro";
 import Start from "./components/StartButton";
 
-const current_deck = MakeDeck();
 createRoot(document.getElementById("root")!).render(
   <>
   <Start />
-  <MakeHand deck={current_deck} handsize={8} />
   </>
 );
