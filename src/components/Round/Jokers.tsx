c34bca9691bf4e874b5432b841f075e12115800c
ASzeto99910 <83287704+AidanS99910@users.noreply.github.com>
2024-12-09 15:58:03 -0500

    jokers now properly render in round

--- src/components/Round/Jokers.tsx
+++ src/components/Round/Jokers.tsx
@@ -6,12 +6,19 @@ interface JokersProps {
 // Renders all owned Jokers onto the screen
 function Jokers({ owned }: JokersProps) {
   const JokerNum = owned.length;
+  console.log(owned);
   return (
     <>
       <img id="jokers" src="https://i.ibb.co/6rg98wD/jokers.png" />
-      {JokerNum >= 1 && <img src={owned[0].img} />}
-      {JokerNum >= 2 && <img src={owned[1].img} />}
-      {JokerNum >= 3 && <img src={owned[2].img} />}
+      {JokerNum >= 1 && (
+        <img id="roundJoker1" className="roundJoker" src={owned[0].img} />
+      )}
+      {JokerNum >= 2 && (
+        <img id="roundJoker2" className="roundJoker" src={owned[1].img} />
+      )}
+      {JokerNum >= 3 && (
+        <img id="roundJoker3" className="roundJoker" src={owned[2].img} />
+      )}
     </>
   );
 }
